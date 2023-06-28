8ac042f7b7b67338f3a9b238d0d61845e377f62e
Justin Verthein <Justinmackay88@hotmail.com>
2023-06-28 10:05:32 -0700

    added models

---
+++ lib/db/models.py
@@ -0,0 +1,36 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class Song(Base):
+    __tablename__ = 'songs'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(255), nullable=False)
+    artist_id = Column(Integer, ForeignKey('artists.id'))
+    album = Column(String(255))
+    genre = Column(String(255))
+    duration = Column(Integer)
+    file_location = Column(String(255))
+
+class Playlist(Base):
+    __tablename__ = 'playlists'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    description = Column(String(255))
+
+class Artist(Base):
+    __tablename__ = 'artists'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    bio = Column(String(255))
+
+class PlaylistSong(Base):
+    __tablename__ = 'playlist_songs'
+
+    playlist_id = Column(Integer, ForeignKey('playlists.id'), primary_key=True)
+    song_id = Column(Integer, ForeignKey('songs.id'), primary_key=True)
