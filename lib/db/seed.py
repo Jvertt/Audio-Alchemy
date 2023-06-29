2d93729897e6e03d24b50442d022c2976cc2025c
Justin Verthein <Justinmackay88@hotmail.com>
2023-06-29 09:44:17 -0700

    seeded artists

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -5,3 +5,23 @@ from models import Song, Playlist, Artist
 
 engine = create_engine('sqlite:///music_library.db')
 Session = sessionmaker(bind=engine)
+
+faker = Faker()
+
+def seed_data():
+    session = Session()
+
+    #artists
+    for i in range(5):
+        name = faker.name()
+        bio = faker.paragraph()
+        artist = Artist(name=name, bio=bio)
+        session.add(artist)
+    
+    session.commit()
+
+    session.close()
+    print("Data seeding completed successfully.")
+
+if __name__ == '__main__':
+    seed_data()
