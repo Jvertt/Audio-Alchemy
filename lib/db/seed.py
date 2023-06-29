88d6541fc04657c431ed8c790eaf7c09bec8375c
Justin Verthein <Justinmackay88@hotmail.com>
2023-06-29 09:39:05 -0700

    created database engine

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -1,1 +1,7 @@
 from faker import Faker 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Song, Playlist, Artist 
+
+engine = create_engine('sqlite:///music_library.db')
+Session = sessionmaker(bind=engine)
