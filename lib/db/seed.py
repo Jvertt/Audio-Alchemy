232f16c066c6b623a482277cdbbaeda02c9e0102
Justin Verthein <Justinmackay88@hotmail.com>
2023-06-29 11:30:39 -0700

    wokring out seeding issues

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -1,55 +1,61 @@
 from faker import Faker
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from datetime import date
+import random
+
 from models import Song, Playlist, Artist
-
-
-engine = create_engine("sqlite:///db/songs.db")
-Session = sessionmaker(bind=engine)
-session = Session()
 
 faker = Faker()
 
+engine = create_engine("sqlite:///songs.db")
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Clear data when session runs
+session.query(Song).delete()
+session.query(Playlist).delete()
+session.query(Artist).delete()
+
 def seed_data():
-    session = Session()
+    # Generate artist data
+    artists = []
+    for i in range(5):
+        artist = Artist(
+            name=faker.name(),
+            bio=faker.paragraph()
+        )
+        artists.append(artist)
 
-    # Seed artists
-    for _ in range(5):
-        name = faker.name()
-        bio = faker.paragraph()
-        artist = Artist(name=name, bio=bio)
-        session.add(artist)
+    # Generate song data
+    songs = []
+    for i in range(10):
+        artist = random.choice(artists)
+        song = Song(
+            title=faker.word(),
+            artist=artist,
+            album=faker.word(),
+            genre=faker.word(),
+            duration=faker.random_int(min=180, max=300)
+        )
+        songs.append(song)
 
+    # Generate playlist data
+    playlists = []
+    for i in range(3):
+        playlist = Playlist(
+            name=faker.word(),
+            description=faker.sentence()
+        )
+        playlists.append(playlist)
+
+    # Add songs to playlists
+    for playlist in playlists:
+        playlist.songs.extend(random.choices(songs, k=5))
+
+    # Add all data to the session
+    session.add_all(artists + songs + playlists)
     session.commit()
-
-    # Seed songs
-    artists = session.query(Artist).all()
-    for _ in range(10):
-        title = faker.word()
-        artist = faker.random.choice(artists)
-        album = faker.word()
-        genre = faker.word()
-        duration = faker.random_int(min=180, max=300)
-        song = Song(title=title, artist=artist, album=album, genre=genre, duration=duration)
-        session.add(song)
-
-    session.commit()
-
-    # Seed playlists
-    songs = session.query(Song).all()
-    for _ in range(3):
-        name = faker.word()
-        description = faker.sentence()
-        playlist = Playlist(name=name, description=description)
-
-        # Add random songs to the playlist
-        playlist.songs.extend(faker.random_choices(songs, length=5))
-        
-        session.add(playlist)
-
-    session.commit()
-
-    session.close()
     print("Data seeding completed successfully.")
 
 if __name__ == '__main__':
